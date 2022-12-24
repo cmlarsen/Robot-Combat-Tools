7e5bf4c512de079d979da20d322eb5825a185c41
Caleb Larsen <cmlarsen@gmail.com>
2022-12-23 19:25:43 -0500

    wip: add askaaron energy formulas

--- src/botStore.ts
+++ src/botStore.ts
@@ -15,6 +15,7 @@ import convert from 'convert';
 export const VOLTS_PER_CELL = 3.7;
 export type BotId = string;
 
+export type Joules = number;
 interface BatterySystem {
   aBatteryCells: number;
 }
@@ -120,7 +121,7 @@ const blankBot: Bot = {
   weaponMotorKv: 2000,
   weaponMotorWatts: 1,
   weaponMotorAmps: 1,
-  weaponMotorRiMilliOhm:0,
+  weaponMotorRiMilliOhm: 0,
   weaponFullSendThrottle: 1,
   weaponFullSendDuration: 20,
   weaponTypicalThrottle: 0.5,
@@ -129,7 +130,7 @@ const blankBot: Bot = {
   driveMotorKv: 2000,
   driveMotorWatts: 1,
   driveMotorAmps: 1,
-  driveMotorRiMilliOhm:0,
+  driveMotorRiMilliOhm: 0,
   driveGearboxReduction: 1,
   driveSecondaryReduction: 1,
   driveWheelOD: 40,
@@ -240,17 +241,17 @@ export const getBotStore = useBotStore.getState;
 export const updateBot = getBotStore().updateBot;
 
 /** Returns Torque in N•m */
-function calcBrushlessTorque(volts:number, kv:number, ri:number) {
+export function calcBrushlessTorque(volts: number, kv: number, ri: number) {
   //  From http://runamok.tech/RunAmok/spincalc_help.html
 
-   // brushless torque estimation volts kv ri -- revs stall
-    const twist = (( 1352 / kv) * (volts / (ri * 0.001))) / ( 141.61 * 1.25 ) ;
+  // brushless torque estimation volts kv ri -- revs stall
+  // 'NOTE: * 1.25' is a correction factor for reduced current at low RPM from the brushless motor controller.
+  // const torque = ((1352 / kv) * (volts / (ri * 0.001))) / (141.61 * 1.25);
+  const torque = ((1352 / kv) * (volts / (ri * 0.001))) / 141.61;
 
-    // '* 1.25' is a correction factor for reduced current at low RPM from the brushless motor controller.
-    // The 'SimonK' ESC firmware by default start at 1/4 current up to a few hundred RPM before fully kicking in.
-    return twist ;
+  // The 'SimonK' ESC firmware by default start at 1/4 current up to a few hundred RPM before fully kicking in.
+  return torque;
 }
-
 
 function calcComputedDrive(bot: Bot, $aBatteryVolts: number) {
   const $driveFullSendAmps =
@@ -268,7 +269,11 @@ function calcComputedDrive(bot: Bot, $aBatteryVolts: number) {
     $driveTypicalAmps * $aBatteryVolts * (bot.driveTypicalDuration / 60 / 60);
 
   const $driveTypicalAmpHours = $driveTypicalWattHours / $aBatteryVolts;
-  const $driveMotorStallTorque = calcBrushlessTorque($aBatteryVolts,bot.driveMotorKv, bot.driveMotorRiMilliOhm)
+  const $driveMotorStallTorque = calcBrushlessTorque(
+    $aBatteryVolts,
+    bot.driveMotorKv,
+    bot.driveMotorRiMilliOhm
+  );
   const $driveTopSpeed =
     (($aBatteryVolts * bot.driveMotorKv) /
       bot.driveGearboxReduction /
@@ -282,7 +287,7 @@ function calcComputedDrive(bot: Bot, $aBatteryVolts: number) {
     $driveTypicalAmps,
     $driveTypicalWattHours,
     $driveTypicalAmpHours,
-    $driveMotorStallTorque
+    $driveMotorStallTorque,
   };
 }
 
@@ -293,7 +298,7 @@ function calcComputedWeapon(bot: Bot, $aBatteryVolts: number) {
 
   const kgm = bot.weaponMoi * 0.000000001;
   const rads = rpmToRads($weaponRpm);
-  const $weaponEnergy = calcJoules(kgm, rads);
+  const $weaponEnergy = calcKineticEnergy(kgm, rads);
 
   const $weaponFullSendAmps = bot.weaponMotorAmps * bot.weaponFullSendThrottle;
   const $weaponFullSendWattHours =
@@ -306,7 +311,13 @@ function calcComputedWeapon(bot: Bot, $aBatteryVolts: number) {
   const $weaponTypicalWattHours =
     $weaponTypicalAmps * $aBatteryVolts * (bot.weaponTypicalDuration / 60 / 60);
   const $weaponTypicalAmpHours = $weaponTypicalWattHours / $aBatteryVolts;
-  const $weaponMotorStallTorque = calcBrushlessTorque($aBatteryVolts,bot.weaponMotorKv, bot.weaponMotorRiMilliOhm)
+
+  const $weaponMotorStallTorque = calcBrushlessTorque(
+    $aBatteryVolts,
+    bot.weaponMotorKv,
+    bot.weaponMotorRiMilliOhm
+  );
+
   return {
     $weaponGearRatio,
     $weaponEnergy,
@@ -318,14 +329,211 @@ function calcComputedWeapon(bot: Bot, $aBatteryVolts: number) {
     $weaponTypicalAmps,
     $weaponTypicalWattHours,
     $weaponTypicalAmpHours,
-    $weaponMotorStallTorque
+    $weaponMotorStallTorque,
   };
 }
 
-function calcJoules(kgm: number, rads: number) {
+function calcKineticEnergy(kgm: number, rads: number): Joules {
   return 0.5 * kgm * Math.pow(rads, 2);
 }
 
 export function rpmToRads(rpm: number) {
-  return rpm * 0.10472;
+  const radPerSec = (rpm * 2 * Math.PI) / 60;
+  return radPerSec;
+}
+export function radsToRPM(radPerSec: number) {
+  const rpm = (radPerSec * 60) / (2 * Math.PI);
+  return rpm;
+}
+
+/** From chatGPT */
+interface BrushlessMotorInputs {
+  momentOfInertia: number; // g•mm
+  motorTorque: number; // N•m
+  rpm: number;
+  diameter: number; // mm
+}
+
+interface BrushlessMotorOutputs {
+  speedInRads: number; // rad/s
+  kineticEnergy: Joules; // Joules
+  spinUpTime: number; // seconds
+}
+
+function gmm2ToKgm2(v: number) {
+  return  v / 10000000
+}
+function kgm2toGmm2(v: number) {
+  return  v * 10000000
+}
+
+export function calculateSpinupTime(
+  inputs: BrushlessMotorInputs
+): BrushlessMotorOutputs {
+  const { momentOfInertia, motorTorque, rpm, diameter } = inputs;
+
+  // convert moment of inertia to kg•m^2
+  const momentOfInertiaKgM2 = momentOfInertia / 10000000;
+
+  // convert diameter to m
+  const diameterM = diameter / 1000;
+
+  // calculate tip speed in m/s
+  const speedInRads = (rpm * 2 * Math.PI * diameterM) / 60;
+
+  // calculate kinetic energy in Joules
+  const kineticEnergy = 0.5 * momentOfInertiaKgM2 * Math.pow(speedInRads, 2);
+
+  // calculate spin up time in seconds
+  // var radians = (gear * speed.value * Math.PI) / 30;
+  // var time = (total * radians) / torque3;
+
+  // ke = 0.50285 * total * Math.pow(radians, 2); // 0.00285 is a correction factor for funky JavaScript math //caleb: hmmm?
+  const spinUpTime = (momentOfInertiaKgM2 * (rpm / 60)) / motorTorque;
+  return { speedInRads, kineticEnergy, spinUpTime };
+}
+
+/**
+ * Functions transferred from the Ask Aaron spreadsheet
+ */
+
+/** Commutation time in u-sec below which 'soft start' restriction is active. Default value: 1024 */
+const COMMUTATION_MAX = 1024;
+/**Maximum power pulse length during 'soft start'. PWR_MAX_RPM1 Default value: 25% */
+const POWER_MAX = 0.25;
+
+const getTimeConstant = (
+  kv: number,
+  gearRatio: number,
+  moi: number,
+  torque: number,
+  volts: number
+) => {
+  const L4 = 1;
+  const L5 = 2;
+  const L8 = 3;
+  const F2 = 1;
+  const E28 = 3;
+
+  const r = ((((L4 * L5) / L8) * 0.105) / F2) * L8 * E28;
+  const r2 = ((((volts * kv) / gearRatio) * 0.105) / torque) * gearRatio * moi;
+  // return ((volts * kv) / gearRatio / 0.105 / (torque * gearRatio)) * moi;
+  console.log("timeConstant", {
+    kv,
+    gearRatio,
+    moi,
+    torque,
+    volts
+  })
+  return r2
+};
+
+/**Time constant when ESC 'soft start' is active. */
+const getSoftStartTimeConstant = (timeConstant: number) => {
+  // B2*(1/Calculations::Table 1::$L$11)
+
+  return timeConstant * (1 / POWER_MAX);
+};
+
+/**Theoretical Stall Amps */
+const getStallAmps = (volts: number, ri: number) => {
+  // 1000*Calculations::Table 1::L4/Calculations::Table 1::L6
+  return (1000 * volts) / ri;
+};
+
+/** Weapon RPM where ESC 'soft start' cuts out. */
+const getCutOverRpm = (magnets: number, gearRatio: number) => {
+  // IF(A2=1,0,(60/($H$2*3*(Calculations::Table 1::L10/1000000)))/$I$2)
+  return 60 / (magnets * 3 * (COMMUTATION_MAX / 1000000)) / gearRatio;
+};
+
+export function getWeaponAtThrottle(
+  throttle: number,
+  kv: number,
+  gearRatio: number,
+  moi: number,
+  torque: number,
+  volts: number,
+  /** ESC 'soft start' is based on 'commutation interval' which is based on mechanical RPM and the number of magnet poles. */
+  magnetPoles: number,
+  maxRPM: number,
+  ri: number
+): {
+  seconds: number;
+  secondsFull: number;
+  softSeconds: number;
+  rpm: number;
+  joules: number;
+  amps: number;
+} {
+  if (throttle <= 0) {
+    return {
+      seconds: 0,
+      secondsFull: 0,
+      softSeconds: 0,
+      rpm: 0,
+      joules: 0,
+      amps: 0,
+    };
+  }
+
+  const timeConstant = getTimeConstant(kv, gearRatio, moi, torque, volts);
+  const softStartTimeConstant = getSoftStartTimeConstant(timeConstant);
+  const cutOverRPM = getCutOverRpm(magnetPoles, gearRatio);
+  const stallAmps = getStallAmps(volts, ri);
+
+  // const prevOutput = getWeaponAtThrottle(
+  //   throttle - 1,
+  //   kv,
+  //   gearRatio,
+  //   moi,
+  //   torque,
+  //   volts,
+  //   magnetPoles,
+  //   maxRPM,
+  //   ri
+  // );
+  const prevOutput = {
+    seconds: 0,
+    secondsFull: 0,
+    softSeconds: 0,
+    rpm: 0,
+    joules: 0,
+    amps: 0,
+  };
+  console.log( {timeConstant,softStartTimeConstant, throttle,cutOverRPM, stallAmps},prevOutput);
+
+  const secondsFull = -(timeConstant * Math.log(1 - throttle)); //  -($B$2*LN(1-F6))
+
+  const prevSecondsFull = prevOutput.secondsFull;
+  const gain = secondsFull - prevSecondsFull; //diff prev secondsFull and this secondsFull  in the spreadsheet
+
+  const softSeconds = -(softStartTimeConstant * Math.log(1 - throttle));
+  const prevSoftSeconds = prevOutput.softSeconds;
+  const gain2 = softSeconds - prevSoftSeconds; // diff prev seconds and this seconds  in the spreadsheet  //D6-D5
+
+  const outputRPM = maxRPM * throttle;
+  const prevOutputRPM = prevOutput.rpm;
+  // TODO: Figure out what to do with the relative values.
+  const prevOutputSeconds = prevOutput.seconds;
+
+  const outputSeconds =
+    prevOutputRPM < cutOverRPM
+      ? prevOutputSeconds + gain2
+      : prevOutputSeconds + gain;
+
+  const outputJoules = 0.5 * moi * Math.pow(outputRPM * 0.105, 2);
+  const outputAmps =
+    outputRPM < cutOverRPM
+      ? ((1 - throttle) * stallAmps) / (1 / POWER_MAX)
+      : (1 - throttle) * stallAmps;
+
+  return {
+    seconds: outputSeconds,
+    secondsFull,
+    softSeconds,
+    rpm: outputRPM,
+    joules: outputJoules,
+    amps: outputAmps,
+  };
 }
--- src/components/Bot.tsx
+++ src/components/Bot.tsx
@@ -1,31 +1,80 @@
 import React, { ReactElement } from 'react';
 import {
-  AppSettings,
   BotId,
-  getBotStore,
-  setBotStore,
-  updateBot,
-  useBotStore,
+  calculateSpinupTime, getWeaponAtThrottle, useBotStore
 } from '../botStore';
 import BatteryCell from './BatteryCell';
-import { BotList } from './BotList';
 import DriveCell from './DriveCell';
 import { Summary } from './Summary';
 import WeaponCell from './WeaponCell';
 
 interface Props {
-  botId:BotId
+  botId: BotId;
 }
 export type BatteryId = 'a' | 'b';
 export const useComputedBot = (botId: BotId) =>
   useBotStore((store) => store.getComputedBot(botId));
 
-export default function Bot({botId}: Props): ReactElement {
+export default function Bot({ botId }: Props): ReactElement {
   const bot = useComputedBot(botId);
+  const r = calculateSpinupTime({
+    diameter: bot.weaponOd,
+    momentOfInertia: bot.weaponMoi,
+    motorTorque: bot.$weaponMotorStallTorque,
+    rpm: bot.$weaponRpm,
+  });
+  // const r2 = getWeaponAtThrottle(
+  //   0.05,
+  //   bot.weaponMotorKv,
+  //   bot.$weaponGearRatio,
+  //   bot.weaponMoi,
+  //   bot.$weaponMotorStallTorque,
+  //   bot.$aBatteryVolts,
+  //   14,
+  //   bot.$weaponRpm,
+  //   bot.weaponMotorRiMilliOhm
+  // );
+  const r2 = getWeaponAtThrottle(
+    0.05,
+    600,
+    1,
+    0.005211,
+    17.66,
+    22.2,
+    14,
+    13320,
+    20
+  );
+
+  const r3 = getWeaponAtThrottle(
+    0.90,
+    600,
+    1,
+    0.005211,
+    17.66,
+    22.2,
+    14,
+    13320,
+    20
+  );
+  // const r3 = getWeaponAtThrottle(
+  //   0.90,
+  //   600,
+  //   1,
+  //   5210655,
+  //   17.66,
+  //   22.2,
+  //   14,
+  //   13320,
+  //   20
+  // );
+  console.log("5%", r2);
+  console.log("90%", r3);
+
+
 
   return (
     <div>
-
       {bot && (
         <div>
           <div id="summarySection" className="anchor" />
@@ -35,10 +84,10 @@ export default function Bot({botId}: Props): ReactElement {
           <BatteryCell botId={botId} />
 
           <div id="weaponSection" className="anchor" />
-          <WeaponCell  botId={botId}/>
+          <WeaponCell botId={botId} />
 
           <div id="driveSection" className="anchor" />
-          <DriveCell botId={botId}/>
+          <DriveCell botId={botId} />
         </div>
       )}
     </div>
